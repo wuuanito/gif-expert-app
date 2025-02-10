f7b21d67390064d32f91d26c0fd47fdddd37ef94
Juan Frauca <juanfrauca@outlook.com>
2025-02-10 17:08:41 +0100

    A casa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { GifExpertApp } from './GifExpertApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <GifExpertApp />
+  </StrictMode>
 )
