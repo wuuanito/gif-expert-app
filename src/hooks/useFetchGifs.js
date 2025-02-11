92797c0098754f31d3e63accf425589913049ca4
Juan Frauca <juanfrauca@outlook.com>
2025-02-11 12:17:48 +0100

    Release

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -2,7 +2,7 @@ import { useState,useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
  export const useFetchGifs = (category) => {
   const [images, setImages] = useState([])
-  comst [isLoading,setIsloading] = useState(true)
+  const [isLoading,setIsloading] = useState(true)
   const getGifImages = async ()=>{
       const newImages = await getGifs(category)
       setImages(newImages)
